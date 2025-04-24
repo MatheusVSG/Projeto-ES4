fb203a160f9bd7ba682906c3e03fc89ef56a0e93
MatheusVSG <siqueiramatheusvitor@gmail.com>
2025-04-23 22:23:22 -0300

    Estrutura de Pastas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponentComponent } from './not-found-component/not-found-component.component';
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
+import { HomeComponent } from './home/home.component';
+import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponentComponent,
+    CadastroClienteComponent,
+    HomeComponent,
+    SideBarComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
